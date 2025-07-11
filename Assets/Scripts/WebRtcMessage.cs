c4de167e7d8e0c62bb352b7bccc0cb571330ca81
Souporno Ghosh <souporno2000@gmail.com>
2025-07-11 12:09:00 +0530

    Added comments to WebRtcMessage.cs

--- Assets/Scripts/WebRtcMessage.cs
+++ Assets/Scripts/WebRtcMessage.cs
@@ -1,11 +1,33 @@
 ﻿using Newtonsoft.Json;
 
+/// <summary>
+/// Represents a WebRTC signaling message used to exchange SDP offers, answers, ICE candidates,
+/// or custom data between peers.
+/// This is serialized and deserialized as JSON for signaling over WebSocket or other channels.
+/// </summary>
 public class WebRtcMessage
 {
-    [JsonProperty("type")] public string Type;
-    [JsonProperty("sdp")] public string Sdp;
-    [JsonProperty("candidate")] public string Candidate;
-    [JsonProperty("sdpMid")] public string SdpMid;
-    [JsonProperty("sdpMLineIndex")] public int SdpMLineIndex;
-    [JsonProperty("data")] public string Data;
+    // Type of message: "offer", "answer", "candidate", "data", etc.
+    [JsonProperty("type")] 
+    public string Type;
+
+    // Session Description Protocol (SDP) data for offer/answer
+    [JsonProperty("sdp")] 
+    public string Sdp;
+
+    // ICE candidate string used for network traversal
+    [JsonProperty("candidate")] 
+    public string Candidate;
+
+    // The media stream identification tag associated with the ICE candidate
+    [JsonProperty("sdpMid")] 
+    public string SdpMid;
+
+    // The index of the media description in SDP this candidate is associated with
+    [JsonProperty("sdpMLineIndex")] 
+    public int SdpMLineIndex;
+
+    // Optional custom data (e.g., chat messages or control messages)
+    [JsonProperty("data")] 
+    public string Data;
 }
